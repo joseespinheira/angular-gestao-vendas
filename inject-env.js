91acd0f3a16ad7e24a7cb760d2e4e0ddf49b527e
José Espinheira <joseespinheira@gmail.com>
2025-04-14 09:56:49 -0300

    Adicionando enviroments

--- inject-env.js
+++ inject-env.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const successColor = "\x1b[32m%s\x1b[0m";
 const checkSign = "\u{2705}";
-const dotenv = require("dotenv").config({ path: "src/.env" });
+const dotenv = require("dotenv").config({ path: ".env" });
 
 const envFile = `export const environment = {
     firebase: {
@@ -18,6 +18,9 @@ const envFile = `export const environment = {
 };
 `;
 let caminho = "./src/environments/environment.ts";
+if (process.env.PRODUCTION === "false") {
+  caminho = "./src/environments/environment.development.ts";
+}
 const targetPath = path.join(__dirname, caminho);
 fs.writeFile(targetPath, envFile, (err) => {
   if (err) {
